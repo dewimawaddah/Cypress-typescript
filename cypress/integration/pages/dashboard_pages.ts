c399d63f4426a1cdcbe022f128164471f827dc64
Dewi Mawaddah <dewimawaddahwarrohmah@gmail.com>
2023-02-22 21:14:27 +0700

    done positive checkout test case

--- cypress/integration/pages/dashboard_pages.ts
+++ cypress/integration/pages/dashboard_pages.ts
@@ -1,8 +1,8 @@
-export class DashboardPage{
-    link_sauceLabsBackpack = 'Sauce Labs Backpack'
+export class DashboardPage {
+  link_sauceLabsBackpack = "Sauce Labs Backpack";
 
-    sauceLabsBackpack(){
-        cy.contains(this.link_sauceLabsBackpack).click()
-        cy.contains('Sauce Labs Backpack').should('be.visible') 
-    }
+  sauceLabsBackpack() {
+    cy.contains(this.link_sauceLabsBackpack).click();
+    cy.contains(this.link_sauceLabsBackpack).should("be.visible");
+  }
 }
